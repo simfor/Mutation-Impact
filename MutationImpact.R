71d48394172b8972a883da2d4b62c7fbec179dac
Simon Forsberg <simon@madartwork.com>
2011-06-22 18:20:45 +0200

    Add: Weight() 	Deals with 2D-structure vector with gaps. Distance() 	Sets NA in D-matrix to default value. This corresponds to gaps in the sequences because of the alignment. 	Some scores are calculated differently. Main() 	Aligns first. Everything downstream works with the aligned sequences.

--- MutationImpact.R
+++ MutationImpact.R
@@ -5,19 +5,18 @@ Main <- function(ff1, ff2){
 	protA <- readFASTA(ff1, strip.descs=TRUE)
 	protB <- readFASTA(ff2, strip.descs=TRUE)
 	
+	#Aligns the two sequences
 	aligned <- Align(protA[[1]]$seq, protB[[1]]$seq)
 	
 	#Retrieves the secondary structure for protA
 	protA_SecondaryStructure <- SecStructure(protA[[1]]$seq, strsplit(toString(pattern(aligned)), "")[[1]] )
 	
 	#Calculates the weight vector
-	W <- Weight(protA_SecondaryStructure[[1]]$PSIPRED$SecondaryStructure, protA_SecondaryStructure[[1]]$PSIPRED$ConfidenceScore)
-	
-	#Converts the alignment sequences to string vectors
-	
+	W <- Weight(protA_SecondaryStructure[[1]], protA_SecondaryStructure[[2]]$PSIPRED$ConfidenceScore)
 	
 	#Calculates the distance between the two sequences
-	dist <- Distance(protA[[1]]$seq, protB[[1]]$seq, W)
+	#dist <- Distance(protA[[1]]$seq, protB[[1]]$seq, W)
+	dist <- Distance(toString(pattern(aligned)), toString(subject(aligned)), W)
 	
 	#print(aligned)
 	#paste("The distance between the sequences: ", dist)
@@ -48,20 +47,25 @@ Distance <- function(seqA, seqB, W){
 
 	#Calculates the absolute distance matrix
 	D <- abs(As-Bs)
+	
+	#Sets all NA elements in D to a default distance. These values corresponds to gaps in the aligned sequences
+	D[is.na(D)] <- 1
 
 	#Calculates the distance scores
 	property_distances <- t(t(D)*W)
-	property_scores <- sqrt(rowSums(t(t(D^2)*W)))
+	property_scores <- rowSums(property_distances) #sqrt(rowSums(t(t(D^2)*W)))
 	merged_prop_distances <- colSums(property_distances)
-	merged_score <- sqrt(sum(t(t(merged_prop_distances^2))))
+	merged_score <- sum(merged_prop_distances) #sqrt(sum(t(t(merged_prop_distances^2))))
 	TotalScore <- sqrt(sum(t(D^2)*W))
 	
-	Result <- list(property_distances=property_distances, property_scores=property_scores, merged_prop_distances=merged_prop_distances, merged_score=merged_score, TotalScore=TotalScore)
+	list(property_distances=property_distances, property_scores=property_scores, merged_prop_distances=merged_prop_distances, merged_score=merged_score, TotalScore=TotalScore)
 }
 
 SecStructure <- function(seq, seq_align){
+	#The function predicts 2D-structure from seq. It returns a 2D-structure vector with inserted gaps matching those in seq_align
 	Structure <- predictPROTEUS(seq, proteus2.organism="euk")
 	seq_2D <- as.character((1:length(seq_align)))
+	confidence <- (1:length(seq_align))
 	position_nr <- 1
 	gap <- 0
 	
@@ -76,7 +80,7 @@ SecStructure <- function(seq, seq_align){
 		}
 		position_nr <- position_nr + 1
 	}
-	list(seq_2D, Structure)
+	list(seq_2D, Structure[[1]])
 }
 
 Weight <- function(Structure, Confidence){
@@ -84,10 +88,15 @@ Weight <- function(Structure, Confidence){
 	#It returns a vector with weights for each position in the AA-sequence
 	weight <- (1:length(Structure))
 	position_nr <- 1
+	gap <- 0
 
 	for(position in Structure){
-		if((position == 'H' || position == 'E') && Confidence[position_nr] > 5 ){
+		if((position == 'H' || position == 'E') && Confidence[position_nr - gap] > 5 ){
 			weight[position_nr] <- 1.5
+		}
+		else if(position == '-'){ #Penalty for insertion in mutated seq
+			weight[position_nr] <- 1.3
+			gap <- gap + 1
 		}
 		else{
 			weight[position_nr] <- 1
